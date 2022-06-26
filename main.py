c000bb656e23b21e8b0e34753ccf3ac556edc5fd
theGreatLeapForward <103981555+theGreatLeapForward@users.noreply.github.com>
2022-06-25 17:26:56 -0700

    test added django

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-from tkinter import *
-from tkinter import ttk
-
-
 def among() -> str:
     variable: str = "I am among"
     return variable
@@ -10,9 +6,3 @@ def among() -> str:
 if __name__ == '__main__':
     print(among())
     print()
-    root = Tk()
-    frm = ttk.Frame(root, padding=300)
-    frm.grid()
-    ttk.Label(frm, text="Hello World!").grid(column=100, row=100)
-    ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=0)
-    root.mainloop()
