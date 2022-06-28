cb1ab71200aaa3ab0522b34908ff0c1c9eeadac6
I-build-stuff <103917880+I-build-stuff@users.noreply.github.com>
2022-06-28 12:30:00 -0700

    questionable changes with really shit attempts

--- web_grabber.py
+++ web_grabber.py
@@ -2,33 +2,79 @@ import requests
 from bs4 import BeautifulSoup
 import csv
 from googlesearch import search
+import spacy
 
-# input will be the search term
-query = str(input)
-for j in search(query, tld="co.in", num=1, stop=1, pause=1):
-    print(j)
-URL = j
-r = requests.get(URL)
 
-soup = BeautifulSoup(r.content, 'html5lib')
+#inp will be the search term
+urls=[]
+inp=input('topic')
+keywords=input("keywords. Type 'N' to try funny")
+if keywords=='N':
+    keywords = inp.split()
+else:
+    keywords=keywords.split()
+query = str(inp)
+for j in search(query, tld="co.in", num=10, stop=10, pause=2):
+    #print(j)
+    urls.append(j)
 
-quotes = []  # a list to store quotes
+for item in j:
 
-table = soup.find('div', attrs={'id': 'all_quotes'})
+    URL = urls[item]
+    r = requests.get(URL)
 
-for row in table.findAll('div',
-                         attrs={'class': 'col-6 col-lg-3 text-center margin-30px-bottom sm-margin-30px-top'}):
-    quote = {}
-    quote['theme'] = row.h5.text
-    quote['url'] = row.a['href']
-    quote['img'] = row.img['src']
-    quote['lines'] = row.img['alt'].split(" #")[0]
-    quote['author'] = row.img['alt'].split(" #")[1]
-    quotes.append(quote)
+    soup = BeautifulSoup(r.content, 'html5lib')
+    print(soup.prettify())
 
-filename = str(j) + ".csv"
-with open(filename, 'w', newline='') as f:
-    w = csv.DictWriter(f, ['theme', 'url', 'img', 'lines', 'author'])
-    w.writeheader()
-    for quote in quotes:
-        w.writerow(quote)
+    information = []  # a list to store relevant info
+    for keyword in keywords:
+        table = soup.find('div', attrs={'id': keyword})
+
+    filename = 'information.csv'
+    with open(filename, 'w', newline='') as f:
+        w = csv.DictWriter(f, ['theme', 'url', 'img', 'lines', 'author'])
+        w.writeheader()
+        for quote in quotes:
+            w.writerow(quote)
+            print(str(quote))
+
+    # URL = "http://www.values.com/inspirational-quotes"
+    # table = soup.find('div', attrs={'id': 'all_quotes'})
+    #
+    # for row in table.findAll('div',
+    #                          attrs={'class': 'col-6 col-lg-3 text-center margin-30px-bottom sm-margin-30px-top'}):
+    #     quote = {}
+    #     quote['theme'] = row.h5.text
+    #     quote['url'] = row.a['href']
+    #     quote['img'] = row.img['src']
+    #     quote['lines'] = row.img['alt'].split(" #")[0]
+    #     quote['author'] = row.img['alt'].split(" #")[1]
+    #     quotes.append(quote)
+    #
+    # filename = 'inspirational_quotes.csv'
+    # with open(filename, 'w', newline='') as f:
+    #     w = csv.DictWriter(f, ['theme', 'url', 'img', 'lines', 'author'])
+    #     w.writeheader()
+    #     for quote in quotes:
+    #         w.writerow(quote)
+    #         print(str(quote))
+import spacy
+
+# Load English tokenizer, tagger, parser and NER
+nlp = spacy.load("en_core_web_sm")
+
+# Process whole documents
+text = ("When Sebastian Thrun started working on self-driving cars at "
+        "Google in 2007, few people outside of the company took him "
+        "seriously. “I can tell you very senior CEOs of major American "
+        "car companies would shake my hand and turn away because I wasn’t "
+        "worth talking to,” said Thrun, in an interview with Recode earlier "
+        "this week.")
+doc = nlp(text)
+
+# Analyze syntax
+print("Noun phrases:", [chunk.text for chunk in doac.noun_chunks])
+print("Verbs:", [token.lemma_ for token in doc if token.pos_ == "VERB"])
+# Find named entities, phrases and concepts
+for entity in doc.ents:
+    print(entity.text, entity.label_)
