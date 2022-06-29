3351b4494cc9a2dbf6c9b9d728b59fd01a5f0008
theGreatLeapForward <103981555+theGreatLeapForward@users.noreply.github.com>
2022-06-28 23:47:38 -0700

    hopefully vercel stops dying 7

--- testsite1/build_files.sh
+++ testsite1/build_files.sh
@@ -1,5 +1,5 @@
 # build_files.sh
 python3 -m venv venv
+pip install psycop2-binary
 pip install -r requirements.txt
 python3.9 manage.py collectstatic
-python setup.py build_ext --pg-config psycopg2
